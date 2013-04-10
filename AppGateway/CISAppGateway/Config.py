a8a7974a4f701e351cd6f1a6ce8ac1a3b12e555f
Konrad Klimaszewski <konrad.klimaszewski@ncbj.gov.pl>
2013-04-10 15:06:36 +0200

    Defined EPRCore service.

--- AppGateway/CISAppGateway/Config.py
+++ AppGateway/CISAppGateway/Config.py
@@ -47,7 +47,7 @@ class Config(dict):
             'removed'
         )
         #: Supported services
-        self.allowed_services = ('Test', 'MultiNest')
+        self.allowed_services = ('Test', 'MultiNest', 'EPRCore')
         self.gate_url_output = 'http://localhost:8000/'
         #: Path to the shared storage used as communication medium with
         #: AppServer
